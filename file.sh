7091470a0c97c997674a18a4a2261ce56151c4d4
Xk4libur <xk4libur@gmail.com>
2025-03-20 10:14:07 +0000

    Update file.sh

--- file.sh
+++ file.sh
@@ -19,7 +19,7 @@ echo -e "\n${turquoiseColour}1)${endColour}${yellowColour} Hack the NASA in 10 s
 
 echo -e "\n${turquoiseColour}2)${endColour}${yellowColour} Ghostfile${endColour}\n"
 
-echo -e "\n${turquoiseColour}3)${endColour}${yellowColour} ¿Kaboom?${endColour}\n"
+echo -e "\n${turquoiseColour}3)${endColour}${yellowColour} ¿Kaboom? -${endColour}${yellowColour}Yes riko, kaboom${endColour}\n"
 
 echo -e "\n${turquoiseColour}4)${endColour}${yellowColour} Go 2 root${endColour}\n"
 
@@ -30,7 +30,17 @@ read option # Esto captura la tecla pulsada con Enter
 # Opciones del menú
 case $option in
     1)
-        echo -e "\n${greenColour}You are going to hack the NASA in 10 seconds${endColour}\n"
+        echo -e "\n${greenColour}Hacking the NASA ...${endColour}\n"
+        sleep 3
+        echo -e "\n${greenColour}Getting access to the database ...${endColour}\n"
+        sleep 3
+        echo -e "\n${greenColour}Files on trafficking of living beings found  ...${endColour}\n"
+        sleep 2
+        echo -e "\n${greenColour}Downloading files ...${endColour}\n"
+        sleep 2
+        echo -e "\n${greenColour}Almost there ...${endColour}\n"
+        sleep 2
+        echo -e "\n${blueColour}Fucking nerd, did you think that this would work?${endColour}\n"
         ;;
     2)
         echo -e "\n${greenColour}You are going to create a ghostfile${endColour}\n"
